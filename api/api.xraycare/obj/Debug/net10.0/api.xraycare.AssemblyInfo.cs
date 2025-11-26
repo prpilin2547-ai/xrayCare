78075d35fd7ccadb0618cd5083f9fb8b48149456
pang <prpilin2547@gmail.com>
2025-11-26 12:03:53 +0700

    update

--- api/api.xraycare/obj/Debug/net10.0/api.xraycare.AssemblyInfo.cs
+++ api/api.xraycare/obj/Debug/net10.0/api.xraycare.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.xraycare")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6695d9079eb3c3257c4ada36dc9ff1a6ab1f19d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ca0de4af1aa1d760b1c13c0170337810ee6409")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.xraycare")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.xraycare")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
